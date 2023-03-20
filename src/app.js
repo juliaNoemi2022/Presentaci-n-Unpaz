e7fb732891c655787fe6ab992b36b40237e02652
juliaNoemi2022 <julianoemi34@gmail.com>
2023-03-20 20:23:53 -0300

    api rest

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-"Hola Mundo"
+
+const express = require('express');
+const req= require('express/lib/request');
+const app = express();
+const PORT = 3001;
+app.get('/api', (req , res )=>{
+    console.log("paso por aca)")
+    res.json({mensaje:'Hola Mundo'}).status(200)
+});
+app.listen(PORT,()=> {console.log('Estoy escuchando en el puerto: + PORT')})
+
